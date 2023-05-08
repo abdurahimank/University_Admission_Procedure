bb0773deb5dc01d48fdfaec7d9f4be3c34574611
ABDURAHIMAN K <laluvga@gmail.com>
2023-05-08 13:26:51 +0530

    Stage 1/7: No one is left behind!

---
+++ main.py
@@ -0,0 +1,6 @@
+# Stage 1/7: No one is left behind!
+mark_1 = float(input())
+mark_2 = float(input())
+mark_3 = float(input())
+print((mark_1 + mark_2 + mark_3) / 3)
+print("Congratulations, you are accepted!")
